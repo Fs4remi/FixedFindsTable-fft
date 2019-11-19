401f4f554dab4edfff51ff5854db46cf7b109f1c
Jonathan Combs <JofCombs@gmail.com>
2019-11-18 22:10:32 -0800

    dummy response added

--- index.py
+++ index.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 @app.route('/', methods=('POST','GET'))
 def index():
 	if request.method == 'POST':
-		return "test"
+		reply = "TEST test TEST"
+		return render_template('response.html', reply=reply)
 	else:
 		return render_template('index.html')
 
