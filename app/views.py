111def7086577b83ae6c46b4cae780a95920ce54
Fatem <f.s4remi@gmail.com>
2020-05-26 20:35:26 -0700

    added POST method

--- app/views.py
+++ app/views.py
@@ -1,7 +1,29 @@
 from app import app 
 
-from flask import render_template
+from flask import render_template, request, redirect
 
-@app.route("/")
+titles =  dict()
+titles["about"]= "Let's vetify the time"
+titles["index"]= "Find out when your final exam is"
+
+
+@app.route("/", methods=["GET","POST"])
 def index():
-    return render_template("index.html")
+    info = "v"
+    if request.method == "POST":
+        req = request.form
+
+        # Since the AM PM thing is a slider- a checkbox really - if ampm is a key with the value 'on' then it is pm
+        # if ampm is not a key, then the user has chosen AM
+        if "ampm" in request.form.keys():
+            print ("PM")
+        print(request.form.get("time"))
+        print(request.form.get("day"))
+
+        return redirect(request.url)
+
+    return render_template("index.html", index = titles["index"], info = info)
+
+@app.route("/about")
+def about():
+    return render_template("about.html", about = titles["about"])
