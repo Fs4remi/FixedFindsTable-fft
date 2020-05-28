e20e49329c742cc0191bf46c09af41c51f1d7c5c
Jonathan Combs <JofCombs@gmail.com>
2020-05-27 17:07:24 -0700

    spaces -> tabs

--- app/views.py
+++ app/views.py
@@ -10,21 +10,21 @@ titles["index"]= "Find out when your final exam is"
 
 @app.route("/", methods=["GET","POST"])
 def index():
-    df, info = scrape("fall")
-    if request.method == "POST":
-        req = request.form
+	df, info = scrape("fall")
+	if request.method == "POST":
+		req = request.form
 
-        # Since the AM PM thing is a slider- a checkbox really - if ampm is a key with the value 'on' then it is pm
-        # if ampm is not a key, then the user has chosen AM
-        if "ampm" in request.form.keys():
-            print ("PM")
-        print(request.form.get("time"))
-        print(request.form.get("day"))
+		# Since the AM PM thing is a slider- a checkbox really - if ampm is a key with the value 'on' then it is pm
+		# if ampm is not a key, then the user has chosen AM
+		if "ampm" in request.form.keys():
+			print ("PM")
+		print(request.form.get("time"))
+		print(request.form.get("day"))
 
-        return redirect(request.url)
+		return redirect(request.url)
 
-    return render_template("index.html", index = titles["index"], info = info)
+	return render_template("index.html", index = titles["index"], info = info)
 
 @app.route("/about")
 def about():
-    return render_template("about.html", about = titles["about"])
+	return render_template("about.html", about = titles["about"])
